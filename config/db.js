0c70a6c8f7d1d6565e3bb5291d73cfc8e777afd0
Rafael Montero <rmonterodev@gmail.com>
2025-12-31 19:43:00 +0400

    First Bootcamp Mongoose Schema

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const colors = require('colors');
 const connectDB = async () => {
     try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {});
-    console.log(`St. Peterburg MongoDB Connected: ${conn.connection.host}`.green.bold);
+    console.log(`St.Peterburg MongoDB Connected: ${conn.connection.host}`.green.bold);
     }
     catch (error) {
         console.error('Error connecting to the database: '.red.bold, `${error.message}`.red.bold);
---
+++ models/Bootcamps.js
@@ -0,0 +1,92 @@
+const mongoose = require('mongoose');
+
+const BootcampSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, 'Please add a name.'],
+        unique: true,
+        trim: true,
+        maxlength: [50, 'Name can not be more than 50 characters.']
+    },
+    slug: String,
+    description: {
+        type: String,
+        require: [true, 'Please add a description.'],
+        trim: true,
+        maxlength: [500, 'Description can not be more than 500 characters.']
+    },
+    website: {
+        type: String,
+        match: [
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/, 
+            'Please use a valid URL with HTTP or HTTPS'
+        ]
+    },
+    phone: {
+        type: String,
+        maxlength: [20, 'Description can not be more than 20 characters.'],
+        match: [
+            /^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/,
+            'Please use a valid Phone.'
+        ]
+    },
+    email: {
+        type: String,
+        match: [
+            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/,
+            'Please use a valid email'
+        ]
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+    },
+    location: {
+        // GeoJSON point
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+            index: '2dsphere'
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        zipcode: String,
+        country: String,
+    },
+    careers: {
+        //Array of strings
+        type: [String],
+        required: true,
+        enum: [
+            'Web Dev',
+            'Mobile Dev',
+            'UI/UX',
+            'Data Sciense',
+            'Business',
+            'Other'
+        ]
+    },
+    averageRating: {
+        type: Number,
+        min: [1, 'Rating must be at least 1'],
+        max: [10, 'Rating can not be more than 10']
+    },
+    averageCost: Number,
+    photo: {
+        type: String,
+        default: 'no-photo.jpg'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
